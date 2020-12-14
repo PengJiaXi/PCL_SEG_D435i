45290a6966a08b98945a30f552ed17901de31557
GuanShangbin <565838103@qq.com>
2020-12-14 15:43:30 +0800

    version0-GUAN

---
+++ src/MinRect/cmake-build-debug/CMakeFiles/MinRect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MinRect.dir/main.cpp.o"
+  "MinRect"
+  "MinRect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MinRect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
